fde753327505445b75812262b593cc04f1b689ef
Charles van Niman <charles@phukish.com>
2018-06-03 16:17:15 -0600

    Add asteroids function

--- version1/asteroid.py
+++ version1/asteroid.py
@@ -2,6 +2,7 @@ import pyglet
 
 from game import load, resources
 
+
 # Load game window.
 game_window = pyglet.window.Window()
 
@@ -10,11 +11,16 @@ score_label = pyglet.text.Label(text='Score: 0', x=10, y=575)
 level_label = pyglet.text.Label(text='An Asteroids Game!',
                                 x=400, y=575, anchor_x='center')
 
+# Draw the player on screen.
+player_ship = pyglet.sprite.Sprite(img=resources.player_image, x=400, y=300)
+
 
 # Bind our on_draw event to the game window.
 @game_window.event
 def on_draw():
     game_window.clear()
+
+    player_ship.draw()
 
     level_label.draw()
     score_label.draw()
